d44eeeeb14806745b5302f7cf2064aef2b52e7b9
rodrigo araujo <rodrigoaraujo990@gmail.com>
2020-11-23 14:44:34 -0300

    fix cors

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,11 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: 'http://186.225.56.122',
+    withCredentials: false,
+    headers: {
+        'Access-Control-Allow-Origin' : '*',
+        'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    }
 });
 
 export default api;
