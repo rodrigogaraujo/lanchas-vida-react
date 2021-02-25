ce8150600ebc80fd285b5292902246a757c82fdd
ighlobr99 <ighlobr@gmail.com>
2021-02-25 10:39:48 -0300

    feat(pages/appointments): list appointments between dates

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:3333',
-    // baseURL: 'https://nodedeploy.lanchasvida.net',
+    // baseURL: 'http://127.0.0.1:3333',
+    baseURL: 'https://nodedeploy.lanchasvida.net',
     withCredentials: false,
     headers: {
         'Access-Control-Allow-Origin' : '*',
